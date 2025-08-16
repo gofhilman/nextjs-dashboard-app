34673d6996793438269e7cce7c7569b5e2d4dc49
Hilman Fikry <hilman12fikry@gmail.com>
2025-08-16 22:45:25 +0700

    feat: implement customers page with pagination and customer fetching logic

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Pagination from "../../ui/invoices/pagination";
+import { fetchFilteredCustomers } from "../../lib/data";
+import CustomersTable from "../../ui/customers/table";
+
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="p-6">
+      <CustomersTable customers={customers} />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={1} />
+      </div>
+    </div>
+  );
 }
